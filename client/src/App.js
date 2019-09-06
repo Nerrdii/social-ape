98d0af74cdc397318d86205ec22bdc1495fed122
Joseph Davis <joseph.davis@hrblock.com>
2019-09-06 11:19:43 -0500

    Add scream card component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import { deepPurple, teal } from '@material-ui/core/colors';
 import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 import Navbar from './components/Navbar';
 import './App.css';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: deepPurple,
+    secondary: teal
+  }
+});
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar></Navbar>
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={home}></Route>
-            <Route exact path="/login" component={login}></Route>
-            <Route exact path="/signup" component={signup}></Route>
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar></Navbar>
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home}></Route>
+              <Route exact path="/login" component={login}></Route>
+              <Route exact path="/signup" component={signup}></Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 

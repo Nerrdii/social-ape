b4e1e5bd625e23dafa019a8d7ac38bbe0ffabc22
Joseph Davis <joseph.n.davis.98@gmail.com>
2019-09-06 19:14:04 -0500

    Add login form

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,13 +1,142 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import withStyles from '@material-ui/core/styles/withStyles';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import icon from '../images/icon.png';
+
+const styles = {
+  form: {
+    textAlign: 'center'
+  },
+  image: {
+    margin: '20px auto 20px auto'
+  },
+  pageTitle: {
+    margin: '10px auto 10px auto'
+  },
+  textField: {
+    margin: '10px auto 10px auto'
+  },
+  button: {
+    marginTop: 20,
+    position: 'relative'
+  },
+  customError: {
+    color: 'red',
+    fontSize: '0.8rem',
+    marginTop: 10
+  },
+  progress: {
+    position: 'absolute'
+  }
+};
 
 class login extends Component {
+  state = {
+    email: '',
+    password: '',
+    loading: false,
+    errors: {}
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({ loading: true });
+
+    const userData = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    axios
+      .post('/login', userData)
+      .then(res => {
+        console.log(res.data);
+        this.setState({ loading: false });
+
+        this.props.history.push('/');
+      })
+      .catch(err => {
+        this.setState({ errors: err.response.data, loading: false });
+      });
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
   render() {
+    const { classes } = this.props;
+    const { errors, loading } = this.state;
+
     return (
-      <div>
-        <h1>Login Page</h1>
-      </div>
+      <Grid container className={classes.form}>
+        <Grid item sm />
+        <Grid item sm>
+          <img src={icon} alt="icon" className={classes.image} />
+          <Typography variant="h2" className={classes.pageTitle}>
+            Login
+          </Typography>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              helperText={errors.email}
+              error={errors.email ? true : false}
+              className={classes.textField}
+              value={this.state.email}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="password"
+              name="password"
+              type="password"
+              label="Password"
+              helperText={errors.password}
+              error={errors.password ? true : false}
+              className={classes.textField}
+              value={this.state.password}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            {errors.general && (
+              <Typography variant="body2" className={classes.customError}>
+                {errors.general}
+              </Typography>
+            )}
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              disabled={loading}
+              className={classes.button}>
+              Login
+              {loading && (
+                <CircularProgress size={30} className={classes.progress} />
+              )}
+            </Button>
+            <br />
+            <small>
+              Don't have an account? Sign up <Link to="/signup">here</Link>
+            </small>
+          </form>
+        </Grid>
+        <Grid item sm />
+      </Grid>
     );
   }
 }
 
-export default login;
+login.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(login);
